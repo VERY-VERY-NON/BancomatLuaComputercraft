fcb3cae830fe168fe519230d75def876fc7f565b
VERY-VERY-NON <148635054+VERY-VERY-NON@users.noreply.github.com>
2025-08-19 03:40:02 +0200

    Update bancomatServer.lua

--- bancomatServer.lua
+++ bancomatServer.lua
@@ -2,6 +2,8 @@
 local accountFile = "conti.txt"
 local accounts = {}
 
+local modemPort = 1
+
 -- Carica dati
 if fs.exists(accountFile) then
     local file = fs.open(accountFile, "r")
@@ -18,20 +20,23 @@ end
 
 -- Ender Modem
 local modem = peripheral.find("modem") or error("Nessun Ender Modem")
-local serverPort = 1
-modem.open(serverPort)
+modem.open(modemPort) -- canale server
+print("Server Bancomat attivo sul canale 1...")
 
-print("Server Bancomat attivo sulla porta "..serverPort)
+local event, side, channel, replyChannel, message, distance
 
 while true do
-    local event, side, senderChannel, replyChannel, message, distance =
-        os.pullEvent("modem_message")
-
+    repeat
+          event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+    until channel == modemPort
+    -- Decodifica il messaggio (deve essere una stringa serializzata)    
     local response = {}
 
-    if message.cmd == "login" then
-        local cardKey = message.cardKey
-        local pin = message.pin
+    local msg = message
+    
+    if msg.cmd == "login" then
+        local cardKey = msg.cardKey
+        local pin = msg.pin
 
         if accounts[cardKey] then
             if accounts[cardKey].pin == pin then
@@ -48,22 +53,22 @@ while true do
             response.saldo = 0
         end
 
-    elseif message.cmd == "saldo" then
-        local cardKey = message.cardKey
+    elseif msg.cmd == "saldo" then
+        local cardKey = msg.cardKey
         response.success = true
         response.saldo = accounts[cardKey] and accounts[cardKey].saldo or 0
 
-    elseif message.cmd == "deposita" then
-        local cardKey = message.cardKey
-        local quanti = message.amount or 0
+    elseif msg.cmd == "deposita" then
+        local cardKey = msg.cardKey
+        local quanti = msg.amount or 0
         accounts[cardKey].saldo = (accounts[cardKey].saldo or 0) + quanti
         salva()
         response.success = true
         response.saldo = accounts[cardKey].saldo
 
-    elseif message.cmd == "preleva" then
-        local cardKey = message.cardKey
-        local quanti = message.amount or 0
+    elseif msg.cmd == "preleva" then
+        local cardKey = msg.cardKey
+        local quanti = msg.amount or 0
         if (accounts[cardKey].saldo or 0) >= quanti then
             accounts[cardKey].saldo = accounts[cardKey].saldo - quanti
             salva()
@@ -79,6 +84,8 @@ while true do
         response.error = "Comando sconosciuto"
     end
 
-    -- Rispondi sempre al canale del client
-    modem.transmit(replyChannel, serverPort, response)
+    -- Invia sempre la risposta al replyChannel del client
+    modem.transmit(replyChannel, 1, response)
+
+    ::continue::
 end
