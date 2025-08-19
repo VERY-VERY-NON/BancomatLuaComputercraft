867bff8594a940debef50ae8a256fc7545dce2c1
VERY-VERY-NON <148635054+VERY-VERY-NON@users.noreply.github.com>
2025-08-19 04:13:55 +0200

    Update bancomatServer.lua

--- bancomatServer.lua
+++ bancomatServer.lua
@@ -2,6 +2,9 @@
 local accountFile = "conti.txt"
 local accounts = {}
 
+local moneyFile = "money.txt"
+local money = {}
+
 local modemPort = 1
 
 -- Carica dati
@@ -11,11 +14,26 @@ if fs.exists(accountFile) then
     file.close()
 end
 
+-- Carica dati
+if fs.exists(moneyFile) then
+    local file = fs.open(moneyFile, "r")
+    money = textutils.unserialize(file.readAll())
+    file.close()
+end
+
 -- Salva dati
 local function salva()
     local file = fs.open(accountFile, "w")
     file.write(textutils.serialize(accounts))
     file.close()
+end
+
+
+-- Salva dati
+local function salvaMoney()
+    local moneyFile = fs.open(moneyFile, "w")
+    moneyFile.write(textutils.serialize(money))
+    moneyFile.close()
 end
 
 -- Ender Modem
@@ -71,12 +89,24 @@ while true do
         local quanti = msg.amount or 0
         if (accounts[cardKey].saldo or 0) >= quanti then
             accounts[cardKey].saldo = accounts[cardKey].saldo - quanti
+            accounts[cardKey].pendingMoney = quanti
             salva()
             response.success = true
             response.saldo = accounts[cardKey].saldo
+    elseif msg.cmd == "registra soldi" then
+        local moneyKey = msg.moneyKey
+        local quanti = msg.amount or 0
+
+        if (accounts[cardKey].pendingMoney or 0) >= quanti then  
+                money[moneyKey].quanti = accounts[cardKey].pendingMoney
+                accounts[cardKey].pendingMoney = 0
+                salva()
+                salvaMoney()
+                response.success = true
+                response.saldo = accounts[cardKey].saldo
         else
             response.success = false
-            response.error = "Saldo insufficiente"
+            response.error = "Tentativo truffaldino! Se credi sia un errore, fai uno screen e contattalo alle autorità"
             response.saldo = accounts[cardKey].saldo
         end
     else
