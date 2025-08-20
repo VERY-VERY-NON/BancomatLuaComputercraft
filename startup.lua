766454c80a29c6c1a9397722c881d9612e8ef4cd
VERY-VERY-NON <148635054+VERY-VERY-NON@users.noreply.github.com>
2025-08-20 15:50:08 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,10 +1,20 @@
-while true do
+local attempt = 0
+while attempt < 10 do
     local ok, err = pcall(function()
-        shell.run(nomeDelTuoProgramma.lua)
+        shell.run("nomeDelPRogramma.lua")
     end)
     if not ok then
         print("Errore: "..tostring(err))
+        attempt = attempt + 1
         sleep(2) -- aspetta un po’ prima di riprovare
     end
 end
 
+if peripheral.find("monitor") then
+    local mon = peripheral.find("monitor")
+    mon.clear()
+    mon.setCursorPos(1,1)
+    mon.write("ERRORE CONTATTARE LE AUTORITA'")
+else
+    print("ERRORE")
+end
