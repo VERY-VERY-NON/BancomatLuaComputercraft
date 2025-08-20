2172c00072e9e78c58fbdaa9499f2aa7cd7e3e13
VERY-VERY-NON <148635054+VERY-VERY-NON@users.noreply.github.com>
2025-08-20 15:42:47 +0200

    Update startUp.lua

--- startUp.lua
+++ startUp.lua
@@ -1,1 +1,10 @@
+while true do
+    local ok, err = pcall(function()
+        shell.run("nomeDelTuoProgramma.lua")
+    end)
+    if not ok then
+        print("Errore: "..tostring(err))
+        sleep(2) -- aspetta un po’ prima di riprovare
+    end
+end
 
